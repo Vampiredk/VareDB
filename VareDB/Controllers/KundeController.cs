6d7fc6cb3fedf38143295162df3b1fbb658cecff
vampiredk2 <simonbreit2@gmail.com>
2022-08-25 19:53:03 +0200

    Add projectnice files.date

--- VareDB/Controllers/KundeController.cs
+++ VareDB/Controllers/KundeController.cs
@@ -19,27 +19,33 @@ namespace VareDB.Controllers
             return Ok(await _context.Kunde.ToListAsync());
         }
         [HttpPost]
-        public async Task<ActionResult<List<Kunde>>> addtype(Kunde kunde, string dato)
+        public async Task<ActionResult<List<Kunde>>> addtype(Kunde kunde)
         {
             //opretter Kunde
             _context.Kunde.Add(kunde);
             await _context.SaveChangesAsync();
 
             //opretter Ordre
-            var ordreid = kunde.Kunde_id;
+            DateOnly dato = new DateOnly();
+            var ordre_kundeid = kunde.Kunde_id;
             Ordre ordre = new Ordre();
-            ordre.Kunde_id = ordreid;
-            ordre.Ordre_date = dato;
+            ordre.Kunde_id = ordre_kundeid;
+            ordre.Ordre_date = dato.ToString();
             _context.Ordre.Add(ordre);
             await _context.SaveChangesAsync();
 
-            /*opretter Ordre_vare
-            foreach (vare vare in Lvare)
+            //opretter Ordre_vare
+            /*var vare_ordreid = ordre.Ordre_id;
+            foreach (vareOrdreModel vare in Lvare)
             {
-
+                Ordre_vare ordre_Vare = new Ordre_vare();
+                ordre_Vare.Ordre_id = vare_ordreid;
+                ordre_Vare.Vare_id = vare.id;
+                ordre_Vare.Antal = vare.antal;
+                ordre_Vare.Pris = vare.pris;
+                _context.Ordre_vare.Add(ordre_Vare);
+                await _context.SaveChangesAsync();
             }*/
-
-
 
             return Ok(await _context.Kunde.ToListAsync());
         }
